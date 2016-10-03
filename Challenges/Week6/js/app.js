a9a60572ac6fa074d2d4faf1dad211731b81ba05
James Link <jamesmlink@gmail.com>
2016-10-02 21:33:03 -0400

    finish week 6 challenge : object oriented js box exercise

--- Challenges/Week6/js/app.js
+++ Challenges/Week6/js/app.js
@@ -9,3 +9,52 @@
  
 //Extra credit
 //Create interactive buttons to decrease or increase the Width and Volume
+
+function Box (height, width) {
+	this.height = height;
+	this.width = width;
+};
+
+Box.prototype.heightChange = function(value) {
+	startingBox.height += value;
+	var newHTML = "Height: " + startingBox.height +"em";
+	$("#height").html(newHTML);
+	var newVolHTML = "Vol: " + (startingBox.width * startingBox.height) +"em";
+	$("#vol").html(newVolHTML);
+};
+
+Box.prototype.widthChange = function(value) {
+	startingBox.width += value;
+	var newHTML = "Width: " + startingBox.width +"em";
+	$("#width").html(newHTML);
+	var newVolHTML = "Vol: " + (startingBox.width * startingBox.height) +"em";
+	$("#vol").html(newVolHTML);
+};
+
+Box.prototype.print = function () {
+	var widthCSS = startingBox.width + "em";
+	var heightCSS = startingBox.height + "em";	$("#boxman").css("width", widthCSS).css("height", heightCSS);
+};
+
+var startingBox = new Box(10,10);
+
+$("#HeightDecrease").click(function() {
+	startingBox.heightChange(-1)
+});
+
+$("#HeightIncrease").click(function() {
+	startingBox.heightChange(1)
+});
+
+$("#WeightDecrease").click(function() {
+	startingBox.widthChange(-1)
+});
+
+$("#WeightIncrease").click(function() {
+	startingBox.widthChange(1)
+});
+
+$("#Print").click(function () {
+	startingBox.print();
+});
+
